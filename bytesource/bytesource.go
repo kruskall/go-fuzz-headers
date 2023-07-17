bc6f5c6c6eea1b2d03e3829fb92b8a2a0729f891
kruskall <99559985+kruskall@users.noreply.github.com>
2023-07-17 05:10:16 +0200

    refactor: move type generation to bytesource and actually use it

--- bytesource/bytesource.go
+++ bytesource/bytesource.go
@@ -15,60 +15,226 @@
 package bytesource
 
 import (
-	"bytes"
 	"encoding/binary"
-	"io"
-	"math/rand"
+	"errors"
+	"math"
 )
 
 type ByteSource struct {
-	*bytes.Reader
-	fallback rand.Source
+	data         []byte
+	dataTotal    uint32
+	position     uint32
+	maxStringLen uint32
 }
 
 // New returns a new ByteSource from a given slice of bytes.
-func New(input []byte) *ByteSource {
+func New(input []byte, maxStringLen uint32) *ByteSource {
 	s := &ByteSource{
-		Reader:   bytes.NewReader(input),
-		fallback: rand.NewSource(0),
-	}
-	if len(input) > 0 {
-		s.fallback = rand.NewSource(int64(s.consumeUint64()))
+		data:         input,
+		dataTotal:    uint32(len(input)),
+		position:     0,
+		maxStringLen: maxStringLen,
 	}
 	return s
 }
 
-func (s *ByteSource) Uint64() uint64 {
-	// Return from input if it was not exhausted.
-	if s.Len() > 0 {
-		return s.consumeUint64()
-	}
-
-	// Input was exhausted, return random number from fallback (in this case fallback should not be
-	// nil). Try first having a Uint64 output (Should work in current rand implementation),
-	// otherwise return a conversion of Int63.
-	if s64, ok := s.fallback.(rand.Source64); ok {
-		return s64.Uint64()
-	}
-	return uint64(s.fallback.Int63())
+func IsDivisibleBy(n int, divisibleby int) bool {
+	return (n % divisibleby) == 0
 }
 
-func (s *ByteSource) Int63() int64 {
-	return int64(s.Uint64() >> 1)
-}
-
-func (s *ByteSource) Seed(seed int64) {
-	s.fallback = rand.NewSource(seed)
-	s.Reader = bytes.NewReader(nil)
-}
-
-// consumeUint64 reads 8 bytes from the input and convert them to a uint64. It assumes that the the
-// bytes reader is not empty.
-func (s *ByteSource) consumeUint64() uint64 {
-	var bytes [8]byte
-	_, err := s.Read(bytes[:])
-	if err != nil && err != io.EOF {
-		panic("failed reading source") // Should not happen.
+func (f *ByteSource) GetInt() (int, error) {
+	if f.position >= uint32(f.dataTotal) {
+		return 0, errors.New("not enough bytes to create int")
 	}
-	return binary.BigEndian.Uint64(bytes[:])
+	returnInt := int(f.data[f.position])
+	f.position++
+	return returnInt, nil
+}
+
+func (f *ByteSource) GetByte() (byte, error) {
+	if f.position >= f.dataTotal {
+		return 0x00, errors.New("not enough bytes to get byte")
+	}
+	returnByte := f.data[f.position]
+	f.position++
+	return returnByte, nil
+}
+
+func (f *ByteSource) GetNBytes(numberOfBytes int) ([]byte, error) {
+	if f.position >= f.dataTotal {
+		return nil, errors.New("not enough bytes to get byte")
+	}
+	returnBytes := make([]byte, 0, numberOfBytes)
+	for i := 0; i < numberOfBytes; i++ {
+		newByte, err := f.GetByte()
+		if err != nil {
+			return nil, err
+		}
+		returnBytes = append(returnBytes, newByte)
+	}
+	return returnBytes, nil
+}
+
+func (f *ByteSource) GetUint16() (uint16, error) {
+	u16, err := f.GetNBytes(2)
+	if err != nil {
+		return 0, err
+	}
+	littleEndian, err := f.GetBool()
+	if err != nil {
+		return 0, err
+	}
+	if littleEndian {
+		return binary.LittleEndian.Uint16(u16), nil
+	}
+	return binary.BigEndian.Uint16(u16), nil
+}
+
+func (f *ByteSource) GetUint32() (uint32, error) {
+	i, err := f.GetInt()
+	if err != nil {
+		return uint32(0), err
+	}
+	return uint32(i), nil
+}
+
+func (f *ByteSource) GetUint64() (uint64, error) {
+	u64, err := f.GetNBytes(8)
+	if err != nil {
+		return 0, err
+	}
+	littleEndian, err := f.GetBool()
+	if err != nil {
+		return 0, err
+	}
+	if littleEndian {
+		return binary.LittleEndian.Uint64(u64), nil
+	}
+	return binary.BigEndian.Uint64(u64), nil
+}
+
+func (f *ByteSource) GetBytes() ([]byte, error) {
+	if f.position >= f.dataTotal {
+		return nil, errors.New("not enough bytes to create byte array")
+	}
+	length, err := f.GetUint32()
+	if err != nil {
+		return nil, errors.New("not enough bytes to create byte array")
+	}
+	if f.position+length > f.maxStringLen {
+		return nil, errors.New("created too large a string")
+	}
+	byteBegin := f.position - 1
+	if byteBegin >= f.dataTotal {
+		return nil, errors.New("not enough bytes to create byte array")
+	}
+	if length == 0 {
+		return nil, errors.New("zero-length is not supported")
+	}
+	if byteBegin+length >= f.dataTotal {
+		return nil, errors.New("not enough bytes to create byte array")
+	}
+	if byteBegin+length < byteBegin {
+		return nil, errors.New("numbers overflow")
+	}
+	f.position = byteBegin + length
+	return f.data[byteBegin:f.position], nil
+}
+
+func (f *ByteSource) GetString() (string, error) {
+	if f.position >= f.dataTotal {
+		return "nil", errors.New("not enough bytes to create string")
+	}
+	length, err := f.GetUint32()
+	if err != nil {
+		return "nil", errors.New("not enough bytes to create string")
+	}
+	if f.position > f.maxStringLen {
+		return "nil", errors.New("created too large a string")
+	}
+	byteBegin := f.position
+	if byteBegin >= f.dataTotal {
+		return "nil", errors.New("not enough bytes to create string")
+	}
+	if byteBegin+length > f.dataTotal {
+		return "nil", errors.New("not enough bytes to create string")
+	}
+	if byteBegin > byteBegin+length {
+		return "nil", errors.New("numbers overflow")
+	}
+	f.position = byteBegin + length
+	return string(f.data[byteBegin:f.position]), nil
+}
+
+func (f *ByteSource) GetBool() (bool, error) {
+	if f.position >= f.dataTotal {
+		return false, errors.New("not enough bytes to create bool")
+	}
+	if IsDivisibleBy(int(f.data[f.position]), 2) {
+		f.position++
+		return true, nil
+	} else {
+		f.position++
+		return false, nil
+	}
+}
+
+// GetStringFrom returns a string that can only consist of characters
+// included in possibleChars. It returns an error if the created string
+// does not have the specified length.
+func (f *ByteSource) GetStringFrom(possibleChars string, length int) (string, error) {
+	if (f.dataTotal - f.position) < uint32(length) {
+		return "", errors.New("not enough bytes to create a string")
+	}
+	output := make([]byte, 0, length)
+	for i := 0; i < length; i++ {
+		charIndex, err := f.GetInt()
+		if err != nil {
+			return string(output), err
+		}
+		output = append(output, possibleChars[charIndex%len(possibleChars)])
+	}
+	return string(output), nil
+}
+
+func (f *ByteSource) GetRune() ([]rune, error) {
+	stringToConvert, err := f.GetString()
+	if err != nil {
+		return []rune("nil"), err
+	}
+	return []rune(stringToConvert), nil
+}
+
+func (f *ByteSource) GetFloat32() (float32, error) {
+	u32, err := f.GetNBytes(4)
+	if err != nil {
+		return 0, err
+	}
+	littleEndian, err := f.GetBool()
+	if err != nil {
+		return 0, err
+	}
+	if littleEndian {
+		u32LE := binary.LittleEndian.Uint32(u32)
+		return math.Float32frombits(u32LE), nil
+	}
+	u32BE := binary.BigEndian.Uint32(u32)
+	return math.Float32frombits(u32BE), nil
+}
+
+func (f *ByteSource) GetFloat64() (float64, error) {
+	u64, err := f.GetNBytes(8)
+	if err != nil {
+		return 0, err
+	}
+	littleEndian, err := f.GetBool()
+	if err != nil {
+		return 0, err
+	}
+	if littleEndian {
+		u64LE := binary.LittleEndian.Uint64(u64)
+		return math.Float64frombits(u64LE), nil
+	}
+	u64BE := binary.BigEndian.Uint64(u64)
+	return math.Float64frombits(u64BE), nil
 }
--- consumer.go
+++ consumer.go
@@ -15,73 +15,32 @@
 package gofuzzheaders
 
 import (
-	"encoding/binary"
-	"errors"
 	"fmt"
-	"math"
 	"reflect"
 	"unsafe"
+
+	"github.com/AdaLogics/go-fuzz-headers/bytesource"
 )
 
 type ConsumeFuzzer struct {
-	data                 []byte
-	dataTotal            uint32
+	source               *bytesource.ByteSource
 	CommandPart          []byte
 	RestOfArray          []byte
 	NumberOfCalls        int
-	position             uint32
 	fuzzUnexportedFields bool
 	curDepth             int
 	Funcs                map[reflect.Type]reflect.Value
 	DisallowUnknownTypes bool
 	MaxDepth             int
-	MaxTotalLen          uint32
-}
-
-func IsDivisibleBy(n int, divisibleby int) bool {
-	return (n % divisibleby) == 0
 }
 
 func NewConsumer(fuzzData []byte) *ConsumeFuzzer {
 	return &ConsumeFuzzer{
-		data:        fuzzData,
-		dataTotal:   uint32(len(fuzzData)),
-		Funcs:       make(map[reflect.Type]reflect.Value),
-		curDepth:    0,
-		MaxDepth:    100,
-		MaxTotalLen: 2000000,
+		source:   bytesource.New(fuzzData, 2000000),
+		Funcs:    make(map[reflect.Type]reflect.Value),
+		curDepth: 0,
+		MaxDepth: 100,
 	}
-}
-
-func (f *ConsumeFuzzer) Split(minCalls, maxCalls int) error {
-	if f.dataTotal == 0 {
-		return errors.New("could not split")
-	}
-	numberOfCalls := int(f.data[0])
-	if numberOfCalls < minCalls || numberOfCalls > maxCalls {
-		return errors.New("bad number of calls")
-	}
-	if int(f.dataTotal) < numberOfCalls+numberOfCalls+1 {
-		return errors.New("length of data does not match required parameters")
-	}
-
-	// Define part 2 and 3 of the data array
-	commandPart := f.data[1 : numberOfCalls+1]
-	restOfArray := f.data[numberOfCalls+1:]
-
-	// Just a small check. It is necessary
-	if len(commandPart) != numberOfCalls {
-		return errors.New("length of commandPart does not match number of calls")
-	}
-
-	// Check if restOfArray is divisible by numberOfCalls
-	if !IsDivisibleBy(len(restOfArray), numberOfCalls) {
-		return errors.New("length of commandPart does not match number of calls")
-	}
-	f.CommandPart = commandPart
-	f.RestOfArray = restOfArray
-	f.NumberOfCalls = numberOfCalls
-	return nil
 }
 
 func (f *ConsumeFuzzer) AllowUnexportedFields() {
@@ -169,7 +128,7 @@ func (f *ConsumeFuzzer) fuzzStruct(e reflect.Value, customFunctions bool) error 
 			}
 		}
 	case reflect.String:
-		str, err := f.GetString()
+		str, err := f.source.GetString()
 		if err != nil {
 			return err
 		}
@@ -185,14 +144,11 @@ func (f *ConsumeFuzzer) fuzzStruct(e reflect.Value, customFunctions bool) error 
 			maxElements = 50
 		}
 
-		randQty, err := f.GetUint32()
+		randQty, err := f.source.GetUint32()
 		if err != nil {
 			return err
 		}
 		numOfElements := randQty % maxElements
-		if (f.dataTotal - f.position) < numOfElements {
-			numOfElements = f.dataTotal - f.position
-		}
 
 		uu := reflect.MakeSlice(e.Type(), int(numOfElements), int(numOfElements))
 
@@ -213,7 +169,7 @@ func (f *ConsumeFuzzer) fuzzStruct(e reflect.Value, customFunctions bool) error 
 			e.Set(uu)
 		}
 	case reflect.Uint16:
-		newInt, err := f.GetUint16()
+		newInt, err := f.source.GetUint16()
 		if err != nil {
 			return err
 		}
@@ -221,7 +177,7 @@ func (f *ConsumeFuzzer) fuzzStruct(e reflect.Value, customFunctions bool) error 
 			e.SetUint(uint64(newInt))
 		}
 	case reflect.Uint32:
-		newInt, err := f.GetUint32()
+		newInt, err := f.source.GetUint32()
 		if err != nil {
 			return err
 		}
@@ -229,7 +185,7 @@ func (f *ConsumeFuzzer) fuzzStruct(e reflect.Value, customFunctions bool) error 
 			e.SetUint(uint64(newInt))
 		}
 	case reflect.Uint64:
-		newInt, err := f.GetInt()
+		newInt, err := f.source.GetInt()
 		if err != nil {
 			return err
 		}
@@ -237,7 +193,7 @@ func (f *ConsumeFuzzer) fuzzStruct(e reflect.Value, customFunctions bool) error 
 			e.SetUint(uint64(newInt))
 		}
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		newInt, err := f.GetInt()
+		newInt, err := f.source.GetInt()
 		if err != nil {
 			return err
 		}
@@ -245,7 +201,7 @@ func (f *ConsumeFuzzer) fuzzStruct(e reflect.Value, customFunctions bool) error 
 			e.SetInt(int64(newInt))
 		}
 	case reflect.Float32:
-		newFloat, err := f.GetFloat32()
+		newFloat, err := f.source.GetFloat32()
 		if err != nil {
 			return err
 		}
@@ -253,7 +209,7 @@ func (f *ConsumeFuzzer) fuzzStruct(e reflect.Value, customFunctions bool) error 
 			e.SetFloat(float64(newFloat))
 		}
 	case reflect.Float64:
-		newFloat, err := f.GetFloat64()
+		newFloat, err := f.source.GetFloat64()
 		if err != nil {
 			return err
 		}
@@ -261,7 +217,7 @@ func (f *ConsumeFuzzer) fuzzStruct(e reflect.Value, customFunctions bool) error 
 			e.SetFloat(float64(newFloat))
 		}
 	case reflect.Bool:
-		newBool, err := f.GetBool()
+		newBool, err := f.source.GetBool()
 		if err != nil {
 			return err
 		}
@@ -273,7 +229,7 @@ func (f *ConsumeFuzzer) fuzzStruct(e reflect.Value, customFunctions bool) error 
 		if e.CanSet() {
 			e.Set(reflect.MakeMap(e.Type()))
 			const maxElements = 50
-			randQty, err := f.GetInt()
+			randQty, err := f.source.GetInt()
 			if err != nil {
 				return err
 			}
@@ -299,7 +255,7 @@ func (f *ConsumeFuzzer) fuzzStruct(e reflect.Value, customFunctions bool) error 
 			return nil
 		}
 	case reflect.Uint8:
-		b, err := f.GetByte()
+		b, err := f.source.GetByte()
 		if err != nil {
 			return err
 		}
@@ -315,235 +271,4 @@ func (f *ConsumeFuzzer) fuzzStruct(e reflect.Value, customFunctions bool) error 
 		}
 	}
 	return nil
-}
-
-func (f *ConsumeFuzzer) GetStringArray() (reflect.Value, error) {
-	// The max size of the array:
-	const max uint32 = 20
-
-	arraySize := f.position
-	if arraySize > max {
-		arraySize = max
-	}
-	stringArray := reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf("string")), int(arraySize), int(arraySize))
-	if f.position+arraySize >= f.dataTotal {
-		return stringArray, errors.New("could not make string array")
-	}
-
-	for i := 0; i < int(arraySize); i++ {
-		stringSize := uint32(f.data[f.position])
-		if f.position+stringSize >= f.dataTotal {
-			return stringArray, nil
-		}
-		stringToAppend := string(f.data[f.position : f.position+stringSize])
-		strVal := reflect.ValueOf(stringToAppend)
-		stringArray = reflect.Append(stringArray, strVal)
-		f.position += stringSize
-	}
-	return stringArray, nil
-}
-
-func (f *ConsumeFuzzer) GetInt() (int, error) {
-	if f.position >= f.dataTotal {
-		return 0, errors.New("not enough bytes to create int")
-	}
-	returnInt := int(f.data[f.position])
-	f.position++
-	return returnInt, nil
-}
-
-func (f *ConsumeFuzzer) GetByte() (byte, error) {
-	if f.position >= f.dataTotal {
-		return 0x00, errors.New("not enough bytes to get byte")
-	}
-	returnByte := f.data[f.position]
-	f.position++
-	return returnByte, nil
-}
-
-func (f *ConsumeFuzzer) GetNBytes(numberOfBytes int) ([]byte, error) {
-	if f.position >= f.dataTotal {
-		return nil, errors.New("not enough bytes to get byte")
-	}
-	returnBytes := make([]byte, 0, numberOfBytes)
-	for i := 0; i < numberOfBytes; i++ {
-		newByte, err := f.GetByte()
-		if err != nil {
-			return nil, err
-		}
-		returnBytes = append(returnBytes, newByte)
-	}
-	return returnBytes, nil
-}
-
-func (f *ConsumeFuzzer) GetUint16() (uint16, error) {
-	u16, err := f.GetNBytes(2)
-	if err != nil {
-		return 0, err
-	}
-	littleEndian, err := f.GetBool()
-	if err != nil {
-		return 0, err
-	}
-	if littleEndian {
-		return binary.LittleEndian.Uint16(u16), nil
-	}
-	return binary.BigEndian.Uint16(u16), nil
-}
-
-func (f *ConsumeFuzzer) GetUint32() (uint32, error) {
-	i, err := f.GetInt()
-	if err != nil {
-		return uint32(0), err
-	}
-	return uint32(i), nil
-}
-
-func (f *ConsumeFuzzer) GetUint64() (uint64, error) {
-	u64, err := f.GetNBytes(8)
-	if err != nil {
-		return 0, err
-	}
-	littleEndian, err := f.GetBool()
-	if err != nil {
-		return 0, err
-	}
-	if littleEndian {
-		return binary.LittleEndian.Uint64(u64), nil
-	}
-	return binary.BigEndian.Uint64(u64), nil
-}
-
-func (f *ConsumeFuzzer) GetBytes() ([]byte, error) {
-	if f.position >= f.dataTotal {
-		return nil, errors.New("not enough bytes to create byte array")
-	}
-	length, err := f.GetUint32()
-	if err != nil {
-		return nil, errors.New("not enough bytes to create byte array")
-	}
-	if f.position+length > f.MaxTotalLen {
-		return nil, errors.New("created too large a string")
-	}
-	byteBegin := f.position - 1
-	if byteBegin >= f.dataTotal {
-		return nil, errors.New("not enough bytes to create byte array")
-	}
-	if length == 0 {
-		return nil, errors.New("zero-length is not supported")
-	}
-	if byteBegin+length >= f.dataTotal {
-		return nil, errors.New("not enough bytes to create byte array")
-	}
-	if byteBegin+length < byteBegin {
-		return nil, errors.New("numbers overflow")
-	}
-	f.position = byteBegin + length
-	return f.data[byteBegin:f.position], nil
-}
-
-func (f *ConsumeFuzzer) GetString() (string, error) {
-	if f.position >= f.dataTotal {
-		return "nil", errors.New("not enough bytes to create string")
-	}
-	length, err := f.GetUint32()
-	if err != nil {
-		return "nil", errors.New("not enough bytes to create string")
-	}
-	if f.position > f.MaxTotalLen {
-		return "nil", errors.New("created too large a string")
-	}
-	byteBegin := f.position
-	if byteBegin >= f.dataTotal {
-		return "nil", errors.New("not enough bytes to create string")
-	}
-	if byteBegin+length > f.dataTotal {
-		return "nil", errors.New("not enough bytes to create string")
-	}
-	if byteBegin > byteBegin+length {
-		return "nil", errors.New("numbers overflow")
-	}
-	f.position = byteBegin + length
-	return string(f.data[byteBegin:f.position]), nil
-}
-
-func (f *ConsumeFuzzer) GetBool() (bool, error) {
-	if f.position >= f.dataTotal {
-		return false, errors.New("not enough bytes to create bool")
-	}
-	if IsDivisibleBy(int(f.data[f.position]), 2) {
-		f.position++
-		return true, nil
-	} else {
-		f.position++
-		return false, nil
-	}
-}
-
-func (f *ConsumeFuzzer) FuzzMap(m interface{}) error {
-	return f.GenerateStruct(m)
-}
-
-// GetStringFrom returns a string that can only consist of characters
-// included in possibleChars. It returns an error if the created string
-// does not have the specified length.
-func (f *ConsumeFuzzer) GetStringFrom(possibleChars string, length int) (string, error) {
-	if (f.dataTotal - f.position) < uint32(length) {
-		return "", errors.New("not enough bytes to create a string")
-	}
-	output := make([]byte, 0, length)
-	for i := 0; i < length; i++ {
-		charIndex, err := f.GetInt()
-		if err != nil {
-			return string(output), err
-		}
-		output = append(output, possibleChars[charIndex%len(possibleChars)])
-	}
-	return string(output), nil
-}
-
-func (f *ConsumeFuzzer) GetRune() ([]rune, error) {
-	stringToConvert, err := f.GetString()
-	if err != nil {
-		return []rune("nil"), err
-	}
-	return []rune(stringToConvert), nil
-}
-
-func (f *ConsumeFuzzer) GetFloat32() (float32, error) {
-	u32, err := f.GetNBytes(4)
-	if err != nil {
-		return 0, err
-	}
-	littleEndian, err := f.GetBool()
-	if err != nil {
-		return 0, err
-	}
-	if littleEndian {
-		u32LE := binary.LittleEndian.Uint32(u32)
-		return math.Float32frombits(u32LE), nil
-	}
-	u32BE := binary.BigEndian.Uint32(u32)
-	return math.Float32frombits(u32BE), nil
-}
-
-func (f *ConsumeFuzzer) GetFloat64() (float64, error) {
-	u64, err := f.GetNBytes(8)
-	if err != nil {
-		return 0, err
-	}
-	littleEndian, err := f.GetBool()
-	if err != nil {
-		return 0, err
-	}
-	if littleEndian {
-		u64LE := binary.LittleEndian.Uint64(u64)
-		return math.Float64frombits(u64LE), nil
-	}
-	u64BE := binary.BigEndian.Uint64(u64)
-	return math.Float64frombits(u64BE), nil
-}
-
-func (f *ConsumeFuzzer) CreateSlice(targetSlice interface{}) error {
-	return f.GenerateStruct(targetSlice)
 }
